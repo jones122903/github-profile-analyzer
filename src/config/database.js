a3c14d4bf67ed33e99f92c9b081026a83aa0c7e5
Terence <terencejones2903@gmail.com>
2026-08-31 23:35:54 +0530

    fix: support cloud MySQL connection

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,13 @@ const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 connection.connect((err) => {
